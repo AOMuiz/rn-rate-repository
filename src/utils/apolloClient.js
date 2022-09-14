1fc7fc1f7086ab7dbc9ebeca403249c2d7415d9b
AOMuiz <abdulwasiumuiz@gmail.com>
2022-09-14 14:08:29 +0100

    add error logging functionnality  in apollo client

--- src/utils/apolloClient.js
+++ src/utils/apolloClient.js
@@ -1,14 +1,35 @@
-import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
+import {
+  ApolloClient,
+  InMemoryCache,
+  createHttpLink,
+  from,
+} from "@apollo/client";
 import Constants from "expo-constants";
+import { onError } from "@apollo/client/link/error";
+
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+  if (graphQLErrors)
+    graphQLErrors.forEach(({ message, locations, path }) =>
+      console.log(
+        `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+      )
+    );
+  if (networkError) console.log(`[Network error]: ${networkError}`);
+});
 
 const httpLink = createHttpLink({
   // Replace the IP address part with your own IP address!
   uri: Constants.manifest.extra.uri,
+  // onError: ({ networkError, graphQLErrors }) => {
+  //   console.log("graphQLErrors", graphQLErrors);
+  //   console.log("networkError", networkError);
+  // },
 });
 
 const createApolloClient = () => {
   return new ApolloClient({
-    link: httpLink,
+    link: from([errorLink, httpLink]),
+    // link: httpLink,
     cache: new InMemoryCache(),
   });
 };
